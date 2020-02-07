216a68e4315e5c7ce7c081298c0cd53624afd411
jpcuve <gexcuj01i>
2020-02-07 15:16:38 +0100

    .

--- app/src/main/java/com/messio/basic/SeizureActivity.kt
+++ app/src/main/java/com/messio/basic/SeizureActivity.kt
@@ -1,5 +1,6 @@
 package com.messio.basic
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -11,12 +12,21 @@ class SeizureActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seizure)
-        intent?.getParcelableExtra<SeizureSummary>("seizure_summary")?.let {
+        val parcelableExtra = intent?.getParcelableExtra<SeizureSummary>("seizure_summary")
+        parcelableExtra?.let {
             Log.d("BASIC", "Intent found: $it")
             it.multiple?.let {
                 oneMoreSeizuresRadioButton.setChecked(it)
                 noSeizureRadioButton.setChecked(!it)
             }
+        }
+        backButton.setOnClickListener {
+            startActivity(Intent(this@SeizureActivity, MainActivity::class.java))
+        }
+        nextButton.setOnClickListener {
+            startActivity(Intent(this@SeizureActivity, SummaryActivity::class.java).apply {
+                putExtra("seizure_summary", parcelableExtra)
+            })
         }
     }
 }
